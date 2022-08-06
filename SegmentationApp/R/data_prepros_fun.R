70b3c32b9fa8eb5a116a6e5e6677a0b79528db63
akins11 <akinwandeayomide24@gmail.com>
2022-08-07 00:26:23 +0100

    Added Missing values and data dimension functions

--- SegmentationApp/R/data_prepros_fun.R
+++ SegmentationApp/R/data_prepros_fun.R
@@ -1,7 +1,101 @@
+#' Check for missing values
+#'
+#' @param df user uploaded data
+#'
+#' @return Boolean
+#' @export
+#'
+#' @examples
+check_for_missing_values <- function(df) {
+  if (is.data.frame(df)) {
+    vapply(df, \(.x) sum(is.na(.x)) > 1, FUN.VALUE = logical(1)) |>
+      any()
+  }
+}
+
+
+
+#' Drop missing values
+#'
+#' @param df user uploaded data 
+#' @param missing_threshold The percentage of missing values a variable can have
+#' to be removed.
+#'
+#' @return data frame
+#' @export
+#'
+#' @examples
+drop_all_missing_values <- function(df, missing_threshold = 75) {
+  vars_with_large_NAs <- vapply(df,
+                                \(.x) (sum(is.na(.x))/nrow(df))*100 >= missing_threshold,
+                                FUN.VALUE = logical(1))
+  
+  f_tbl <- df[!vars_with_large_NAs]
+  f_tbl[complete.cases(f_tbl), ]
+}
+
+
+
+
+#' Check if data is Usable
+#'
+#' @param clean_df  A data.frame without any missing value
+#' @param original_df_nrows Number of rows of the original dataset
+#'
+#' @return
+#' @export
+#'
+#' @examples 
+check_usable_data_frame <- function(clean_df, original_df_nrows) {
+  if (!is.data.frame(clean_df)) {
+    FALSE
+  } else if (is.data.frame(clean_df)) {
+    percent_rows <- (nrow(clean_df) / original_df_nrows)*100
+    if (percent_rows <= 10) {
+      if (nrow(clean_df) < 30) {
+        FALSE
+      } else {
+        TRUE
+      }
+    } else if (ncol(clean_df) < 2) {
+      FALSE
+    } else {
+      TRUE
+    }
+  } 
+}
+
+
+
+#' Data dimension
+#'
+#' @param df data.frame
+#'
+#' @return string
+#' @export
+#'
+#' @examples
+get_data_dimension <- function(df) {
+  if (is.data.frame(df)) {
+    n_row <- nrow(df)
+    n_col <- ncol(df)
+    
+    row_lab <- ifelse(n_row <= 1, "row", "rows") 
+    col_lab <- ifelse(n_col <= 1, "column", "columns")
+    pref <- ifelse(n_row <= 1, "Data have", "There are")
+    
+    stringr::str_glue(
+      "{pref} **{scales::comma(n_row)}** {row_lab} & **{scales::comma(n_col)}** \\
+       {col_lab}"
+    )
+  }
+}
+
+
 #' Variable selection
 #'
 #' @param df data.frame
-#' @param variables character: Variables from the data to use.
+#' @param variables character: Variables from the data.
 #'
 #' @return data.frame
 #' @export
@@ -12,10 +106,7 @@ select_input_vars <- function(df, variables) {
     shiny::validate("A minimum of two variables must be selected to continue")
   }
   
-  f_tbl <- df |>
-    dplyr::select(tidyselect::all_of(variables))
-  
-  return(f_tbl)
+  dplyr::select(df, tidyselect::all_of(variables))
 }
 
 
@@ -87,7 +178,10 @@ prepare_cluster_data <- function(df,
   }
   
   # drop dates ----------------------------------------------------------------|
-  df <- df[vapply(df, \(.x) !lubridate::is.Date(.x), FUN.VALUE =  logical(1))]
+  is.date <- function(x) {
+    inherits(x, c("Date", "POSIXt"))
+  }
+  df <- df[vapply(df, \(.x) !is.date(.x), FUN.VALUE = logical(1))]
   
   
   if (include_factors) {
@@ -188,7 +282,8 @@ push_to_cluster_alg <- function(df,
 #' @examples
 drop_data_id <- function(df, id_var = "No Selection") {
   if (id_var != "No Selection") {
-    dplyr::select(df, -tidyselect::all_of(id_var))
+    f_tbl <- dplyr::select(df, -tidyselect::all_of(id_var))
+    stats::na.omit(f_tbl)
   } else {
     df
   }
